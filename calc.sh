cc098ae6b41d43f3ee38d737033226b5afdfafd5
DESKTOP-0V9VAH6\ashok <ashok.chakravarthi@zohocorp.com>
2021-11-16 21:58:32 +0530

    Calculator

--- calc.sh
+++ calc.sh
@@ -1,11 +1,59 @@
 #! /usr/bin/env bash
+while true
 
-# When the program is first loaded, display a greeting to the user.
-# Then, display a menu that outlines the possible operations:
+do
+  # When the program is first loaded, display a greeting to the user.
+  echo " Welcome to my Calculator"
+
+  # Then, display a menu that outlines the possible operations:
   # Add
   # Subtract
   # Exit
-# Then, capture the user selection.
-# If the selection matches a supported operation, execute the operation.
-# If the selection does not match a support operation, display an error message.
-# When the operation is complete, redisplay the menu.
+  echo "Select any of the following operations to perform.
+    1) Addition - a
+    2) Subtraction - s
+    3) Division - d
+    4) Multiplication - m
+    Press e to Quit."
+
+  # Then, capture the user selection.
+  echo "Please Enter your selection:"
+  read OPERATION
+  
+    # If the selection matches a supported operation, execute the operation.
+    # If the selection does not match a support operation, display an error message.
+    case $OPERATION in
+            "addition"|"a")
+
+                    let "result= $1+$2"
+                    echo "Result: $result"
+                    ;;
+
+            "subtraction"|"s")
+
+                    let "result= $1-$2"
+                    echo "Result: $result"
+                    ;;
+
+            "division"|"d")
+
+                    let "result = $1/$2"
+                    echo "Result: $result"
+                    ;;
+
+            "multiplication"|"m")
+
+                    let "result=$1*$2"
+                    echo "Result: $result"
+                    ;;
+
+            "exit"||"e")
+
+                    echo "Bye Bye!!"
+                    break
+                    ;;
+             *)
+                    echo "Please check your selection"
+                    ;;
+        esac
+done
